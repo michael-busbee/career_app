532f7ad1e6e32a20ec1ba11fe7cf1bf299a17d14
Michael Busbee <mbusbee505@gmail.com>
2025-03-05 21:52:36 -0500

    Setup Complete

---
+++ app/layout.tsx
@@ -0,0 +1,42 @@
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import React from "react";
+
+import "./globals.css";
+
+const inter = localFont({
+  src: "./fonts/interVF.ttf",
+  variable: "--font-inter",
+  weight: "100 200 300 400 500 600 700 800  900",
+});
+
+const spaceGrotesk = localFont({
+  src: "./fonts/spaceGroteskVF.ttf",
+  variable: "--font-space-grotesk",
+  weight: "300 400 500 600 700",
+});
+
+export const metadata: Metadata = {
+  title: "CareerFlow",
+  description:
+    "A community driven platform for career guidance. Get help, share knowledge, and collaborate with other jobseekers.",
+  icons: {
+    icon: "/images/site-logo.svg",
+  },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${inter.className} ${spaceGrotesk.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+const Home = () => (
+  <>
+    <h1 className="h1-bold font-inter text-primary-500">
+      Welcome to the world of Next.js
+    </h1>
+    ;
+    <h1 className="h1-bold font-space-grotesk text-primary-500">
+      Welcome to the world of Next.js
+    </h1>
+    ;
+  </>
+);
+
+export default Home;
